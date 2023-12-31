LMODroid/platform_frameworks_lmodroid

framework: Add applock
Changes are imported from the original frameworks/base commit
Ie35c6c7103b8bed644d510d8268d6c99cb71a1a2 and adapted to the
lmodroid framework.

Dhina17: Adapted for android 14 and lmo framework.

Change-Id: Ie79d1edb497133b7553841f748ca159c3a4a447c
Co-authored-by: Dhina17 <dhinalogu@gmail.com>

core/java/com/libremobileos/applock/AppLockData.aidl
@@ -1,13 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-/**
- * Copyright (C) 2023 The LibreMobileOS Foundation
+/*
+ * Copyright (C) 2022 FlamingoOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
--->
 
-<resources>
-    <!-- External LMODroid specific core services -->
-    <java-symbol type="array" name="config_externalLMODroidServices" />
+package com.libremobileos.applock;
 
-    <!-- App lock -->
-    <java-symbol type="string" name="unlock_application" />
-    <java-symbol type="array" name="config_appLockAllowedSystemApps" />
-
-</resources>
+parcelable AppLockData;

DerpFest-AOSP/frameworks_derpfest

core/java/org/derpfest/applock/AppLockData.aidl
@@ -1,13 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-/**
- * Copyright (C) 2023 The LibreMobileOS Foundation
+/*
+ * Copyright (C) 2022 FlamingoOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,14 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
--->
 
-<resources>
-    <!-- External DerpFest specific core services -->
-    <java-symbol type="array" name="config_externalDerpFestServices" />
+package org.derpfest.applock;
 
-    <!-- App lock -->
-    <java-symbol type="string" name="unlock_application" />
-    <java-symbol type="array" name="config_appLockAllowedSystemApps" />
-
-</resources>
+parcelable AppLockData;