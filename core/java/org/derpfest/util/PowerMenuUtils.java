LMODroid-TMP/platform_frameworks_lmodroid

core: Add a panic trigger to Global actions
Change-Id: I212554ca697fb66c67a4460df61eadcbc5e4144a

core/java/com/libremobileos/util/PowerMenuUtils.java
@@ -17,11 +17,15 @@
 package com.libremobileos.util;
 
 import android.app.KeyguardManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 
 import com.libremobileos.providers.LMOSettings;
+import static com.libremobileos.util.PowerMenuConstants.*;
 
 public final class PowerMenuUtils {
     public static boolean isAdvancedRestartPossible(final Context context) {
@@ -32,5 +36,16 @@ public final class PowerMenuUtils {
         boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_SYSTEM;
 
         return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
+    }
+
+    public static boolean isPanicAvailable(final Context context) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(PANIC_PACKAGE, PANIC_ACTIVITY));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (context.getPackageManager().resolveActivity(intent,
+                PackageManager.MATCH_SYSTEM_ONLY) != null) {
+            return true;
+        }
+        return false;
     }
 }

DerpFest-AOSP/frameworks_derpfest

core/java/org/derpfest/util/PowerMenuUtils.java
@@ -17,11 +17,19 @@
 package org.derpfest.util;
 
 import android.app.KeyguardManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.UserHandle;
 import android.provider.Settings;
 
+<<<<<<< HEAD:core/java/org/derpfest/util/PowerMenuUtils.java
 import org.derpfest.providers.DerpFestSettings;
+=======
+import org.derpfest.providers.DerpFestSettings;
+import static org.derpfest.util.PowerMenuConstants.*;
+>>>>>>> c98f5ab (core: Add a panic trigger to Global actions):core/java/org.derpfest/util/PowerMenuUtils.java
 
 public final class PowerMenuUtils {
     public static boolean isAdvancedRestartPossible(final Context context) {
@@ -32,5 +40,16 @@ public final class PowerMenuUtils {
         boolean isPrimaryUser = UserHandle.getCallingUserId() == UserHandle.USER_SYSTEM;
 
         return advancedRestartEnabled && !keyguardLocked && isPrimaryUser;
+    }
+
+    public static boolean isPanicAvailable(final Context context) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(PANIC_PACKAGE, PANIC_ACTIVITY));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        if (context.getPackageManager().resolveActivity(intent,
+                PackageManager.MATCH_SYSTEM_ONLY) != null) {
+            return true;
+        }
+        return false;
     }
 }